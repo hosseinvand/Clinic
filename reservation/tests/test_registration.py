78541605fb803f8b52df00aa99d2dcbfc6ddda03
Hossein Hosseinvand <h.hosseinvand@gmail.com>
2016-12-23 22:54:27 +0330

    new office test

--- reservation/tests/test_registration.py
+++ reservation/tests/test_registration.py
@@ -8,8 +8,43 @@ def create_test_user(username, password):
     return User.objects.create_user(username=username, email='ahmad@gmail.com', password=password,
                              first_name='ahmad', last_name='ahmad')
 
+def create_test_doctor(doctor_code, username, password):
+    user = create_test_user(username=username, password=password)
+    doctor = Doctor.objects.create(doctor_code=doctor_code, education='S', speciality='Jarahi', insurance='Iran', price=35000, cv='maybe not the best doc in the world but the happiest one :)', contract='contracts/')
+    return SystemUser.objects.create(user=user, id_code='123456', role=doctor)
+
+class OfficeAddTest(TestCase):
+    def test_page_status_for_anonymous_user(self):
+        self.client.logout()
+        response = self.client.get(reverse_lazy('addClinic'))
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_page_status_for_non_doctor_user(self):
+        tmp_user = create_test_user(username='ahmad', password='password')
+        SystemUser.objects.create(user=tmp_user, id_code='123456', role=None)
+        self.client.logout()
+        self.client.login(username='ahmad', password='password')
+        response = self.client.get(reverse_lazy('addClinic'))
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_add_new_offic(self):
+        office_data = {
+            'city': 'Zanjan',
+            'address': 'میدان انقلاب',
+            'phone': '0223344213',
+            'telegram': 'ahmadClinic',
+        }
+        user = create_test_doctor('123456', 'ahmad', 'password')
+        self.client.logout()
+        self.client.login(username='ahmad', password='password')
+        response = self.client.post(reverse_lazy('addClinic'), office_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(user.role.offices.all().filter(address=office_data['address'], phone=office_data['phone']).exists())
+
+
 class DoctorSignupViewTest(TestCase):
     def test_page_status_for_anonymous_user(self):
+        self.client.logout()
         response = self.client.get(reverse_lazy('doctorRegister'))
         self.assertNotEqual(response.status_code, 200)
 
@@ -24,7 +59,7 @@ class DoctorSignupViewTest(TestCase):
     def test_valid_new_doctor_creation(self):
         doctor_data = {
             'doctor_code': '123456',
-            'education': 'Yale University',
+            'education': 'S',
             'speciality': 'Eye',
             'insurance': 'Iran',
             'price': 35000,
@@ -37,7 +72,7 @@ class DoctorSignupViewTest(TestCase):
     def test_invalid_new_doctor_code_already_exist(self):
         doctor_data = {
             'doctor_code': '123456',
-            'education': 'Yale University',
+            'education': 'S',
             'speciality': 'Eye',
             'insurance': 'Iran',
             'price': 35000,
