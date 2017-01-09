c2dd05b97b4a5e4030dac7f26834aec23fde72ae
Hossein Hosseinvand <h.hosseinvand@gmail.com>
2017-01-09 17:59:24 +0330

    comment if

--- clinicHeroku/urls.py
+++ clinicHeroku/urls.py
@@ -24,11 +24,11 @@ urlpatterns = [
     url(r'^', include('reservation.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
+# else:
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
