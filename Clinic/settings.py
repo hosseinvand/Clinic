23336bdf0e4f4b9151a781cf92a3fd7a95b17f52
Hossein Hosseinvand <h.hosseinvand@gmail.com>
2017-01-09 19:48:27 +0330

    fix settings

--- Clinic/settings.py
+++ Clinic/settings.py
@@ -1,20 +1,159 @@
 
+HEROKU = False
+
 import os
 import dj_database_url
 
+# if HEROKU:
+#
+#     # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#
+#
+#     # Quick-start development settings - unsuitable for production
+#     # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+#
+#     # SECURITY WARNING: keep the secret key used in production secret!
+#     SECRET_KEY = "+p9ygq*3pvy_60^gl3r@62*zi2v0cl&lmci2!-^%nc9+nmfkg+"
+#
+#     # SECURITY WARNING: don't run with debug turned on in production!
+#     DEBUG = True
+#
+#     # Application definition
+#
+#     INSTALLED_APPS = [
+#         'polymorphic',
+#         'material',
+#         'widget_tweaks',
+#         'django.contrib.admin',
+#         'django.contrib.auth',
+#         'django.contrib.contenttypes',
+#         'django.contrib.sessions',
+#         'django.contrib.messages',
+#         # Disable Django's own staticfiles handling in favour of WhiteNoise, for
+#         # greater consistency between gunicorn and `./manage.py runserver`. See:
+#         # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
+#         'whitenoise.runserver_nostatic',
+#         'django.contrib.staticfiles',
+#         'reservation',
+#         'multiselectfield',
+#     ]
+#
+#     MIDDLEWARE_CLASSES = [
+#         'django.middleware.security.SecurityMiddleware',
+#         'whitenoise.middleware.WhiteNoiseMiddleware',
+#         'django.contrib.sessions.middleware.SessionMiddleware',
+#         'django.middleware.common.CommonMiddleware',
+#         'django.middleware.csrf.CsrfViewMiddleware',
+#         'django.contrib.auth.middleware.AuthenticationMiddleware',
+#         'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+#         'django.contrib.messages.middleware.MessageMiddleware',
+#         'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     ]
+#
+#     ROOT_URLCONF = 'Clinic.urls'
+#
+#     TEMPLATES = [
+#         {
+#             'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#             'DIRS': [os.path.join(BASE_DIR, 'templates')],
+#             'APP_DIRS': True,
+#             'OPTIONS': {
+#                 'context_processors': [
+#                     'django.template.context_processors.debug',
+#                     'django.template.context_processors.request',
+#                     'django.contrib.auth.context_processors.auth',
+#                     'django.contrib.messages.context_processors.messages',
+#                 ],
+#                 'debug': DEBUG,
+#             },
+#         },
+#     ]
+#
+#     WSGI_APPLICATION = 'Clinic.wsgi.application'
+#
+#
+#     # Database
+#     # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+#
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+#
+#     AUTH_PASSWORD_VALIDATORS = [
+#         {
+#             'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#         },
+#         {
+#             'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#         },
+#         {
+#             'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#         },
+#         {
+#             'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#         },
+#     ]
+#
+#     # Internationalization
+#     # https://docs.djangoproject.com/en/1.9/topics/i18n/
+#
+#     LANGUAGE_CODE = 'en-us'
+#     TIME_ZONE = 'UTC'
+#     USE_I18N = True
+#     USE_L10N = True
+#     USE_TZ = True
+#
+#     # Update database configuration with $DATABASE_URL.
+#     db_from_env = dj_database_url.config(conn_max_age=500)
+#     DATABASES['default'].update(db_from_env)
+#
+#     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+#     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#
+#     # Allow all host headers
+#     ALLOWED_HOSTS = ['*']
+#
+#     # Static files (CSS, JavaScript, Images)
+#     # https://docs.djangoproject.com/en/1.9/howto/static-files/
+#
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+#     STATIC_URL = '/static/'
+#
+#     # Extra places for collectstatic to find static files.
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static'),
+#     ]
+#
+#     LOGIN_URL = '/login/'
+#     LOGOUT_REDIRECT_URL = '/login/'
+#
+#     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#     MEDIA_URL = '/media/'
+#
+#     # Simplified static file serving.
+#     # https://warehouse.python.org/project/whitenoise/
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#
+# else:
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "+p9ygq*3pvy_60^gl3r@62*zi2v0cl&lmci2!-^%nc9+nmfkg+"
+SECRET_KEY = '&me6(fjwvc%v866rjwx$#u%2y8gw5j5^xt^cu1pc+m4=_59+oz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -27,23 +166,18 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # Disable Django's own staticfiles handling in favour of WhiteNoise, for
-    # greater consistency between gunicorn and `./manage.py runserver`. See:
-    # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'reservation',
     'multiselectfield',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -62,23 +196,25 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'debug': DEBUG,
         },
     },
 ]
 
 WSGI_APPLICATION = 'Clinic.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test.db')
     }
 }
+
+# Password validation
+# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -96,41 +232,34 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
-STATIC_URL = '/static/'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-LOGIN_URL = '/login/'
-LOGOUT_REDIRECT_URL = '/login/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+
+LOGIN_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/login/'
