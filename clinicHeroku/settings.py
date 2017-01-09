fa1e35c16a5a6b7bb73089155171824b866dc9f2
Hossein Hosseinvand <h.hosseinvand@gmail.com>
2017-01-09 17:48:26 +0330

    template

--- clinicHeroku/settings.py
+++ clinicHeroku/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'clinicHeroku.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,10 +76,6 @@ TEMPLATES = [
         },
     },
 ]
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-)
 
 WSGI_APPLICATION = 'clinicHeroku.wsgi.application'
 
