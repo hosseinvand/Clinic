4dd770cf96260c38229ab48dc0fff3684a4367b9
Hossein Hosseinvand <h.hosseinvand@gmail.com>
2017-01-09 17:32:31 +0330

    change url

--- clinicHeroku/settings.py
+++ clinicHeroku/settings.py
@@ -127,8 +127,8 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfile')
+STATIC_URL = '/staticfile/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
