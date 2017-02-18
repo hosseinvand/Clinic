4d8ea499022750bbc972c63c22098c495a216b0f
Hossein Hosseinvand <h.hosseinvand@gmail.com>
2017-02-18 17:16:14 +0330

    commit to merge

--- notebook/urls.py
+++ notebook/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
+from django.urls import reverse_lazy
 
 from notebook.views import MainPageView, Login, DoctorsView, DoctorDetailView
 
 urlpatterns = [
+    url(r'^logout/$', logout, {'next_page': "/notebook/login"}, name='react_logout'),
     url(r'^api/login/$', Login.as_view(), name='react_login'),
     url(r'^api/doctors/$', DoctorsView.as_view(), name='react_doctors'),
     url(r'^api/doctor/(?P<pk>\d+)/$', DoctorDetailView.as_view(), name='react_doctors'),
--- reactjs/containers/Layout.js
+++ reactjs/containers/Layout.js
@@ -1,9 +1,48 @@
 import React, { Component, PropTypes } from 'react'
 
 class Layout extends Component {
+    // renderNavbar() {
+    //     return (
+    //         <div class="navbar navbar-default">
+    //             <div class="container-fluid">
+    //                 <div class="navbar-header">
+    //                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+    //                         <span class="icon-bar"></span>
+    //                         <span class="icon-bar"></span>
+    //                         <span class="icon-bar"></span>
+    //                     </button>
+    //                     <a class="active navbar-brand" href="{% url 'react_home' %}">کلینیک</a>
+    //                 </div>
+    //                 <div class="navbar-collapse collapse navbar-responsive-collapse">
+    //                     <div id="navbar-links">
+    //                         <ul class="nav navbar-nav">
+    //                             <li><a href="/notebook/doctors">پزشکان</a></li>
+    //                             <li><a href="/panel">مشاهده نوبت‌ها</a></li>
+    //                         </ul>
+    //                     </div>
+    //                     <ul class="nav navbar-nav navbar-left">
+    //                         {% if user.is_authenticated %}
+    //                             <li>
+    //                                 <a href="#" >
+    //                                     {{ user.first_name }}
+    //                                     به کلینیک خوش آمدید
+    //                                 </a>
+    //                             </li>
+    //                             <li><a href="{% url "react_logout" %}">خروج</a></li>
+    //                         {% else %}
+    //                             <li><a href="/notebook/login">ورود</a></li>
+    //                         {% endif %}
+    //                     </ul>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     )
+    // }
+
     render() {
         return (
             <div>
+                {/*{this.renderNavbar()}*/}
                 {this.props.children}
             </div>
         )
