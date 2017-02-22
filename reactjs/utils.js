fedeefdf22e25823e25d50ac860ed74a81364a16
Hossein Hosseinvand <h.hosseinvand@gmail.com>
2017-02-22 19:00:02 +0330

    fix url

--- reactjs/utils.js
+++ reactjs/utils.js
@@ -1,4 +1,4 @@
-const API_ROOT = "http://localhost:3000/notebook/api/"
+const API_ROOT = "https://asd-clinic.herokuapp.com/notebook/api/"
 
 export function getFullUrl(endpoint) {
     const fullUrl = (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint
